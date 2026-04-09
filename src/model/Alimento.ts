a8c5eccf4882c418f9176aa75eb3ca0d36cb922a
Lucas <lucaaszstr@gmail.com>
2026-04-09 16:47:22 -0300

    refactor: muda o encapsulamento dos atributos e adiciona getters e setters

--- src/model/Alimento.ts
+++ src/model/Alimento.ts
@@ -1,8 +1,24 @@
 import Produto from "./Produto";
 
 export default class Alimento extends Produto{
-    public validade: string;
-    public perecivel: boolean;
+    private validade: string;
+    private perecivel: boolean;
+
+    public getValidade(): string {
+        return this.validade;
+    }
+
+    public getPerecivel(): boolean {
+        return this.perecivel;
+    }
+
+    public setValidade(validade: string): void {
+        this.validade = validade;
+    }
+
+    public setPerecivel(perecivel: boolean): void {
+        this.perecivel = perecivel;
+    }
 
     public exibirInfo(): string {
         return `[Alimento] ${this.nome} | R$${this.preco.toFixed(2)} | Qtd: ${this.quantidade} | Validade: ${this.validade} | Perecível: ${this.perecivel ? "Sim" : "Não"}`;
